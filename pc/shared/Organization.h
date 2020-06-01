55699aa88f297fd72f84e68bad703920f32f2265
Michael Bazzinotti <mbazzinotti@gmail.com>
2020-05-31 23:32:19 -0400

    [app-version] switch to major/minor/micro versioning

--- pc/shared/Organization.h
+++ pc/shared/Organization.h
@@ -2,5 +2,10 @@
 
 #define COMPANY_NAME "Black Hole Studios"
 #define APP_NAME "SNES Tracker"
-#define APP_VERSION "0.01"
+#define APP_VER_MAJ 0
+#define APP_VER_MIN 1
+#define APP_VER_MIC 0
+#define APP_VER_STRING(maj, min, mic) #maj "." #min "." #mic
+#define APP_VER_STRING2(maj, min, mic) APP_VER_STRING(maj, min, mic)
+#define APP_VERSION APP_VER_STRING2(APP_VER_MAJ, APP_VER_MIN, APP_VER_MIC)
 #define APP_NAME_VERSION APP_NAME " " APP_VERSION
